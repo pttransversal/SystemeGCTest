ef6a174ed73a8e299185f75fda7b7551091e58da
pttransversal <amal.elaalyani@uit.ac.ma>
2022-03-22 14:22:36 +0100

    first commit

---
+++ src/main/java/org/gestioncontact/SystemeGcTestApplication.java
@@ -0,0 +1,13 @@
+package org.gestioncontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeGcTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeGcTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gestioncontact/SystemeGcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gestioncontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeGcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
